fd971053f74ae02f5c17fb2bb901ef753e09be9b
David Chisnall <David.Chisnall@microsoft.com>
2020-04-24 10:24:28 +0000

    Update #includes of moved header.

--- src/env/__init_tls.c
+++ src/env/__init_tls.c
@@ -7,7 +7,7 @@
 #include "libc.h"
 #include "atomic.h"
 #include "syscall.h"
-#include "shared/sgxlkl_config.h"
+#include "enclave/sgxlkl_config.h"
 #include "enclave/lthread.h"
 
 static int spawned_ethreads = 1;
