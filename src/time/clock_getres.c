6a274388ff86b447313d0e0bf66dba43265566e8
Sean T. Allen <v-seall@microsoft.com>
2020-09-28 13:11:29 +0000

    Revert "Remove no longer needed init_clock_res"
    This reverts commit 1673c3e1b6f8fb162ce60bebcd7869ff1a0b1f04.

--- src/time/clock_getres.c
+++ src/time/clock_getres.c
@@ -1,6 +1,28 @@
 #include <time.h>
+#include "syscall.h"
 
 /*
   CLOCK_REALTIME (1) to CLOCK_BOOTTIME (7)
 */
 #define CLOCK_RES_MAX CLOCK_BOOTTIME
+struct timespec host_clock_res[CLOCK_RES_MAX + 1];
+
+void init_clock_res(struct timespec clock_res[]) {
+	for (int i = 0; i <= CLOCK_RES_MAX; i++) {
+		host_clock_res[i] = clock_res[i];
+	}
+}
+
+int clock_getres(clockid_t clk, struct timespec *ts)
+{
+	/* Unsupported for now */
+	if (clk == CLOCK_PROCESS_CPUTIME_ID ||
+	    clk == CLOCK_THREAD_CPUTIME_ID)
+		return -1;
+
+	if (clk <= CLOCK_RES_MAX) {
+		*ts = host_clock_res[clk];
+		return 0;
+	}
+	return syscall(SYS_clock_getres, clk, ts);
+}
